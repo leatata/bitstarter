97db55ab954d37ab1133fbf58c2ee8428bb9d8ea
Leandro Taberner <leatata@gmail.com>
2013-07-08 19:38:17 +0000

    Web.js read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //var textWeb = fs.readFileSync('index.html', 'utf-8');
+  var bufferWeb = fs.readFileSync('index.html');
+  var textWeb = bufferWeb.toString();
+  response.send(textWeb);
 });
 
 var port = process.env.PORT || 5000;
